c0ed04d72a1774a2ef2bf08b557f50c3749b679f
EdersonDav <ederson.silva@sunoresearch.com.br>
2022-03-07 19:40:18 -0300

    First commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import 'dotenv/config'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res)=>{
+  res.json({message: 'hello'})
+})
+
+app.listen(process.env.PORT, ()=>{
+  console.log(`Server is running in port ${process.env.PORT}`)
+})
